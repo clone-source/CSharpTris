fc9cf538d4eacaf834020662a534c6358b407950
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-11-14 16:38:59 +0100

    run Form from within the Game

--- Engine/Form.cs
+++ Engine/Form.cs
@@ -4,13 +4,35 @@ using Forms = System.Windows.Forms;
 
 namespace CSharpTris.Engine
 {
+    class Player
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; }
+        public float Height { get; }
+
+        public Player(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+
     public sealed class Form : Forms.Form
     {
         private new int Width;
         private new int Height;
-        
+        readonly Bitmap _buffer;
+        readonly Graphics _bufferG;
+
+        private readonly Player _player;
+
         public Form(int width, int height)
         {
+            _player = new Player(0, 0, 20, 20);
+
             Width = width;
             Height = height;
             Text = @"Pong";
@@ -19,6 +41,9 @@ namespace CSharpTris.Engine
             FormBorderStyle = FormBorderStyle.Fixed3D;
             DoubleBuffered = true;
 
+            _buffer = new Bitmap(Width, Height);
+            _bufferG = Graphics.FromImage(_buffer);
+
             SetStyle(
                 ControlStyles.AllPaintingInWmPaint |
                 ControlStyles.OptimizedDoubleBuffer |
@@ -26,10 +51,30 @@ namespace CSharpTris.Engine
                 true);
         }
 
+        public new void Update()
+        {
+            const float dt = 15 / 1000f;
+
+            _player.X += 55 * dt;
+            _player.Y += 111 * dt;
+
+            if (_player.X > Width)
+            {
+                _player.X = -_player.Width;
+            }
+
+            if (_player.Y > Height)
+            {
+                _player.Y = -_player.Height;
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.FillRectangle(Brushes.Brown, 20, 20, 20, 20);
+            _bufferG.Clear(Color.White);
+            _bufferG.FillRectangle(Brushes.Brown, _player.X, _player.Y, 20, 20);
+            g.DrawImage(_buffer, 0, 0, Width, Height);
         }
     }
 }
--- Engine/Game.cs
+++ Engine/Game.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
 namespace CSharpTris.Engine
 {
     public class Game
@@ -7,12 +10,77 @@ namespace CSharpTris.Engine
         public int Width { get; }
         public int Height { get; }
 
+        private Thread _loopThread;
+        private ManualResetEvent _closeGameEvent;
+        private const int FrameSize = 15;
+
         public Game(int width, int height)
         {
             Width = width;
             Height = height;
-            
             Form = new Form(width, height);
+
+            Form.Load += OnLoad;
+            Form.Closed += OnClosed;
+        }
+
+        void OnLoad(object sender, EventArgs e)
+        {
+            _closeGameEvent = new ManualResetEvent(false);
+            _loopThread = new Thread(Loop) {Name = "Game Thread"};
+            _loopThread.Start();
+        }
+
+        void OnClosed(object sender, EventArgs e)
+        {
+            _closeGameEvent.Set();
+            _loopThread.Join();
+            _closeGameEvent.Close();
+        }
+
+        private bool IsStuck(IAsyncResult invokerResult)
+        {
+            if (invokerResult.AsyncWaitHandle.WaitOne(0)) return false;
+
+            // invokerResult exists but the wait handle is not ready yet
+            // it's been `FrameSize` milliseconds already so we to get us out of the slowness
+
+            // if both _resetEvent and invokerResult's WaitHandle are nowhere to be seen
+            // let's get out of the loop
+            return WaitHandle.WaitAny(
+                new[]
+                {
+                    _closeGameEvent,
+                    invokerResult.AsyncWaitHandle
+                }) == 0;
+        }
+
+        private void Loop()
+        {
+            // invoker is in charge of getting a handle on the Main Thread and invoke BuildFrame
+            IAsyncResult invokerResult = null;
+            var running = true;
+
+            while (running)
+            {
+                if (invokerResult != null && IsStuck(invokerResult))
+                {
+                    return;
+                }
+
+                // Now tell main thread to build the frame
+                invokerResult = Form.BeginInvoke(new MethodInvoker(BuildFrame));
+
+                // wait for the remainder of the frame and then let the loop loop
+                // if _closeGameEvent was set, stop the loop
+                running = !_closeGameEvent.WaitOne(FrameSize);
+            }
+        }
+
+        private void BuildFrame()
+        {
+            Form.Update();
+            Form.Invalidate();
         }
     }
 }
